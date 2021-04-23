ae0f1e4c38994696354cae206409f34d66860267
tanghao <tang.hao2@husky.neu.edu>
2021-04-23 05:55:26 -0700

    update server, remove outdated controllers

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ app.use((req, res, next) => {
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-require('./controllers/quizzes-controller')(app)
-require('./controllers/questions-controller')(app)
-require('./controllers/quiz-attempts-controller')(app)
 require('./controllers/user-controller')(app)
 require('./controllers/comment-controller')(app)
 
